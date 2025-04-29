466b2bb2505b12553567261792c664d439758da0
Nguyễn Tuyền <tuyennguyenlt95@gmail.com>
2025-04-29 19:59:08 +0700

    Day 26

---
+++ playwright.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+    testDir: './tests',
+    projects: [
+        {
+            name: 'Chromium',
+            use: {...devices['Desktop Chrome']}
+        }
+    ],
+    use: {
+        headless: false
+    }
+})
